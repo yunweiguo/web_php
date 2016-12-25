f9bb93a9b4f3a5a10a1f3adb0a81241cbc00fef3
yun <345172961@qq.com>
2016-12-25 21:00:53 +0800

    sql

---
+++ student.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2016-12-25 13:57:25
+-- 服务器版本： 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `sex` varchar(2) NOT NULL,
+  `no` varchar(20) NOT NULL,
+  `class` varchar(20) NOT NULL,
+  `address` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `student`
+--
+
+INSERT INTO `student` (`id`, `name`, `sex`, `no`, `class`, `address`) VALUES
+(1, 'yunweiguo', '男', '2013210557', '2013211119', '海淀'),
+(2, '钟远智', '男', '2013219', '2013211', '四川子'),
+(13, 'yun', '男', '1230', '230', '2321');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
